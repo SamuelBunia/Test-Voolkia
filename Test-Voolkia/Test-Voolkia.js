e7aa8f0928ad55a01c55d4c65ed90d09b4128e3a
SamuelBunia <68014549+SamuelBunia@users.noreply.github.com>
2020-11-12 14:26:11 -0300

    Test editado y Explicado

--- Test-Voolkia/Test-Voolkia.js
+++ Test-Voolkia/Test-Voolkia.js
@@ -8,23 +8,32 @@ publicado, " name " de la categoría.*/
 
 
 function mostrar() {
-
+    //pedir que se ingrese el site y el id del usuario
     let sellerId = prompt("Ingrese el seller Id: ");
     let siteId = prompt("Ingrese el site Id: ");
-    //let sellerId = 179571326;
-    //let siteId = "MLA";
+    /*let sellerId = 179571326;
+    let siteId = "MLA";*/
+
 
     const Http = new XMLHttpRequest();
+    //al ingresarlo, lo toma en la url
     const url = `https://api.mercadolibre.com/sites/${siteId}/search?seller_id=${sellerId}`;
     Http.open("GET", url);
     Http.send();
     
+    //espera a que este listo, para empezar a ejecutar
     Http.onreadystatechange = (e) => 
     {
         let responseJson = JSON.parse(Http.responseText);
         console.log(responseJson);
+        //recorrer cada elemento con toda su informacion hasta llegar al final
         responseJson.results.forEach(element => {
         console.log(element);
         });
     }
 }
+
+/*Para buscar la solucion al ejercicio pido un usuario y contraseña para plantearlo a nivel de cliente servidor
+a su vez estos datos obtenidos, son lo que se van a obtener por medio de la url con las variables.
+Con el Http.onreadystatechange espero a que el programa este listo para ser ejecutado. Despues
+con un bucle recorro y muestro cada elemento para ser mostrado*/
